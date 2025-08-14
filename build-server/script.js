55854b09fe6dceb7e57d09bb529909dae056bd66
Daksh Sawhney <darkstardaksh@gmail.com>
2025-08-14 09:44:20 +0530

    completed script.js of build-server

--- build-server/script.js
+++ build-server/script.js
@@ -8,10 +8,10 @@ const mime = require('mime-types')
 
 // Creating S3 Client -- providing communication things
 const s3Client = new S3Client({
-    region: '',
+    region: 'ap-south-1',
     credentials: {
-        accessKeyId: '',
-        secretAccessKey: ''
+        accessKeyId: process.env.AccessKeyId,
+        secretAccessKey: process.env.SecretAccessKey
     }
 })
 
@@ -23,9 +23,11 @@ async function init() {
     const outputDir = path.join(__dirname, 'output')    // join this script folder with output i.e. /home/app -- __dirname and /output
 
     // Go inside folder and run npm install and npm build
+    console.log('Output Directory:', outputDir)
     const p = exec(`cd ${outputDir} && npm install && npm run build`)
 
     // Produce all logs as output
+    console.log('Running command:', `cd ${outputDir} && npm install && npm run build`)
     p.stdout.on('data', (data) => {
         console.log(data.toString())    // it is a buffer, convert to string
     })
@@ -35,28 +37,33 @@ async function init() {
         console.error('Error:', error.toString())    // it is a buffer, convert to string
     })
 
+    console.log('Waiting for build to finish...')
     // Wait for the process to finish
     p.on('close', async() => {
-        console.log('Build Complete')
+        console.log('Build finished successfully!')
         
         // Npm run build makes dist/ folder, containing all static files
         const distFolderPath = path(__dirname, 'output', 'dist')
         const distFolderContents = fs.readdirSync(distFolderPath, {recursive: true})
 
+        console.log('Contents of dist folder:', distFolderContents)
         // Loop over all files and send them to s3 (not folder)
         for(const item of distFolderContents){
             if(fs.lstatSync(item).isDirectory()) continue;
 
             // create config to upload to s3
+            console.log('Processing file:', item)
             const command = new PutObjectCommand({
-                Bucket: '',
+                Bucket: 'vercel-clone-mega-project',
                 Key: `__outputs/${projectID}/${item}`,    // The path, file is stored inside s3
                 Body: fs.createReadStream(item),     // it divides file into chunks for uploading large objects easily
                 ContentType: mime.lookup(item)
             })
 
             // Start uploading to S3
+            console.log('Uploading file to S3:', item)
             await s3Client.send(command)
+            console.log('File uploaded successfully:', item)
 
         }
     })
