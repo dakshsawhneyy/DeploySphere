8f0ed6792ebc96427e5810ebf96bd5043e773737
Daksh Sawhney <darkstardaksh@gmail.com>
2025-08-21 12:51:32 +0530

    Completed API Server

--- api-server/index.js
+++ api-server/index.js
@@ -10,7 +10,7 @@ require('dotenv').config()
 const { Kafka } = require('kafkajs')
 const { createClient } = require('@clickhouse/client')
 
-const { z } = require('zod')    // Powerful Library for validation
+const { z, string } = require('zod')    // Powerful Library for validation
 
 const { PrismaClient } = require('@prisma/client')
 
@@ -64,19 +64,19 @@ const consumer = kafka.consumer({ groupId: `api-server-group` })    // Name of C
 // subscriber.on('end', () => console.log('Redis connection closed'))
 
 // ! Creating Socker Server
-const io = new Server({ cors:'*' })     // Creating new socket server
+// const io = new Server({ cors:'*' })     // Creating new socket server
 
 // Joining Redis Channel for live updates
-io.on('connection', socket => {
-    // Subscribe to Redis
-    socket.on('subscribe', channel => {     // if user wants to subscribe, specify the channel and socket will make him join that channel
-        socket.join(channel)
-        socket.emit('message', `Joined: ${channel}`)    // in events in postman, subscribe to message as message is event
-    })
-    console.log('Socket connected')
-})
+// io.on('connection', socket => {
+//     // Subscribe to Redis
+//     socket.on('subscribe', channel => {     // if user wants to subscribe, specify the channel and socket will make him join that channel
+//         socket.join(channel)
+//         socket.emit('message', `Joined: ${channel}`)    // in events in postman, subscribe to message as message is event
+//     })
+//     console.log('Socket connected')
+// })
 
-io.listen(9002, () => console.log('Socket server listening on port 9002'))
+// io.listen(9002, () => console.log('Socket server listening on port 9002'))
 
 
 // * Creating a new ECS Client
@@ -104,7 +104,7 @@ app.post('/project', async(req,res) => {
     // schema for validating
     const schema = z.object({
         name: z.string(),
-        gitURL: z.string()   // zod will validate the URL
+        gitURL: z.string().url()   // zod will validate the URL
     })
 
     const safeParseResult = schema.safeParse(req.body);
@@ -124,6 +124,8 @@ app.post('/project', async(req,res) => {
     return res.json({ status: 'success', data: project })
 })
 
+
+// ! Route for deploying project by taking projectID in req.body
 app.post('/deploy', async(req,res) => {
     // const { projectId } = req.body // but we need to validate, so we can
 
@@ -192,6 +194,28 @@ app.post('/deploy', async(req,res) => {
     return res.json({ status: 'queued', data: { "project": project, "deployment": deployment }})
 })
 
+
+// ! Route for fetching Logs
+app.get('/logs/:id', async (req, res) => {
+    const id = req.params.id;
+
+    // fetching logs from clickhouse based on deployment_id
+    const logs = await clickhouse.query({
+        query: `SELECT event_id, deployment_id, log, timestamp FROM log_events WHERE deployment_id = {deployment_id:String}`,
+        query_params: { deployment_id: id },
+        format: 'JSONEachRow'
+    })
+
+    // console.log('logs', logs)
+    
+    const rawLogs = await logs.json()
+    console.log('logs:', rawLogs)
+
+    // Returning Logs
+    return res.json({ status: 'success', logs: rawLogs })
+}) 
+
+
 // This will subscribe to redis, which will furthur send logs to socket
 // function subscribeToRedis() {
 //     // Subscribe to Redis Channel
@@ -220,21 +244,28 @@ async function kafkaConsumer() {
             for (const message of messages) {
                 // Converting message into JS Object fron JS String
                 const stringMessage = message.value.toString();     // it is in buffer, converting it into string
-                const { PROJECT_ID, DEPLOYMENT_ID, log } = JSON.parse(stringMessage);    // parsing the string to JSON
+                const { PROJECT_ID, DEPLOYMENT_ID, log, subDomain } = JSON.parse(stringMessage);    // parsing the string to JSON
             
-                // Inserting into clickhouse
-                const {query_id} = await clickhouse.insert({    // inserting as per the table
-                    table: 'log_events',
-                    values: [{ event_id: uuid(), deployment_id: DEPLOYMENT_ID, log: log }],
-                    format: 'JSONEachRow'
-                })
+                try {
+                    // Inserting into clickhouse
+                    const {query_id} = await clickhouse.insert({    // inserting as per the table
+                        table: 'log_events',
+                        values: [{ event_id: uuid(), deployment_id: DEPLOYMENT_ID, log: log }],
+                        format: 'JSONEachRow'
+                    })
 
-                // console.log({query_id})
+                    console.log({log, subDomain, DEPLOYMENT_ID})
+                    // console.log(query_id)
 
-                // Offset Handling -- sequence no. in Kafka. It helps remember kafka where we had left off
-                commitOffsetsIfNecessary(message.offset)    // telling Kafka: “I’m done with all messages up to this offset. You don’t need to send them again if I crash.”
-                resolveOffset(message.offset)     // mark messages as processed (local)
-                await heartbeat();  // if consumer goes quiet for too long, kafka kicks it out of the group
+                    // Offset Handling -- sequence no. in Kafka. It helps remember kafka where we had left off
+                    commitOffsetsIfNecessary(message.offset)    // telling Kafka: “I’m done with all messages up to this offset. You don’t need to send them again if I crash.”
+                    resolveOffset(message.offset)     // mark messages as processed (local)
+                    await heartbeat();  // if consumer goes quiet for too long, kafka kicks it out of the group
+                    // console.log(`Log inserted into ClickHouse: ${log}`)
+                } catch (error) {
+                    console.error('Error inserting log into ClickHouse:', error)
+                }
+                
             }
         }
     })
